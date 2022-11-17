f5e19e87c41e29c70893097fa6743e09b44eb8c8
Zhenwei Yang <yangzw1996@gmail.com>
2022-11-17 15:18:03 +0100

    update functionality
    add compliance rate and documentation

--- R/asim.R
+++ R/asim.R
@@ -1,41 +1,62 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' A simulation Function based on ICJM fitted on PASS data
+#'
+#' This function allows you to simulate the outcomes (censoring, progression, early treatment) of patients in AS based on PSA value and baseline age and PSA density.
+#' @import MASS
+#' @import JMbayes
+#' @import Matrix
+#' @import splines
+#' @import truncnorm
+#' @param n number of patients on active surveillance.
+#' @param seed seed value for reproducibility The default value is 100.
+#' @param param_list the estimated parameters from the PASS data and the fitted ICJM:
+#' \code{t.max}: maximum follow-up time in the PASS data;
+#' \code{mean.Cens}: mean censoring time in the PASS data;
+#' \code{knot.longi}: knots used in the natural cubic spline specification of the longitudinal model;
+#' \code{knot.surv}: knots used in the P-splines of the baseline hazard in the survival model;
+#' \code{betas}: coefficients for the fixed effects in the longitudinal submodel for PSA;
+#' \code{sigma.y}: the standard deviation for the residuals in the longitudinal submodel for PSA;
+#' \code{D_c3}: the covariance matrix for the random effects in the longitudinal submodel for PSA;
+#' \code{gambh}: coefficients for the P-spline design matrix of the baseline hazard in the survival submodel;
+#' \code{gammas}: coefficients for the exogenous covariates in the survival submodel;
+#' \code{alpha}: coefficients for the impact from PSA on the time-to-event outcomes in the survival submodel;
+#' \code{age.mean}: mean age observed in the PASS data (centered around 62);
+#' \code{age.sd}: standard deviation of age observed in the PASS data;
+#' \code{density.mean}: mean of the PSA density (in log) observed in the PASS data;
+#' \code{density.sd}: standard deviation of the PSA density (in log) observed in the PASS data;
+#' \code{cvisit.sep}: regular clinical visit interval for PSA measurement. The default is 3 months (0.25 years);
+#' \code{cvisit.sd}: standard deviation of variation in the clinical visit time.
+#' @param Pcomp the compliance rate of PSA measurements. The default value is 100%.
+#' @param Bcomp the compliance rate of biopsies. The default value is 100%.
+#' @keywords Simulation based on an Interval-censored Cause-specific Joint Model
+#' @return two datasets
+#' @examples
+#' asim()
+#' @export
+
 
 asim <- function(n = 1000, seed = 100,
                  param_list = list(
                    t.max = 12.48,
+                   mean.Cens = 5.114849,
                    knot.longi = c(0, 1.36, 2.96, 12.48),
                    knot.surv = c(0, 0, 0, 0, 1.05, 1.82, 2.41, 3.13, 3.91, 4.67, 5.56, 6.93, 12.48, 12.48, 12.48, 12.48),
                    betas = c(2.3428173, 0.2790604, 0.6051217, 0.9545555, 0.0162572),
                    sigma.y = 0.1452483,
-                   gammas = c(0.4987653, 0.2340489),
-                   alpha = matrix(c(0.1275125, 0.4207432, 3.010636, 2.621581), 2, 2),
-                   gambh = matrix(c(-6.775537, -4.716391, -2.835004, -1.649737, -1.541211, -1.791381, -1.846360, -1.746593, -1.854139, -2.041937, -2.182281, -2.315315,
-                                    -5.755751, -4.994471, -4.431788, -4.261764, -4.356832, -4.474064, -4.600834, -4.686764, -4.778267, -4.917803, -5.076011, -5.209827), 12, 2),
-                   mean.Cens = 5.114849,
                    D_c3 = matrix(c(0.48387474, -0.04027017, -0.07481436,  0.01722258,
                                    -0.04027017, 0.77008166,  0.46077603, -0.04049686,
                                    -0.07481436,  0.46077603,  1.37021094,  1.36480027,
                                    0.01722258, -0.04049686,  1.36480027,  2.53766219), 4, 4),
+                   gambh = matrix(c(-6.775537, -4.716391, -2.835004, -1.649737, -1.541211, -1.791381, -1.846360, -1.746593, -1.854139, -2.041937, -2.182281, -2.315315,
+                                    -5.755751, -4.994471, -4.431788, -4.261764, -4.356832, -4.474064, -4.600834, -4.686764, -4.778267, -4.917803, -5.076011, -5.209827), 12, 2),
+                   gammas = c(0.4987653, 0.2340489),
+                   alpha = matrix(c(0.1275125, 0.4207432, 3.010636, 2.621581), 2, 2),
                    age.mean = -0.1248499,
                    age.sd = 6.86713,
                    density.mean = -2.272663,
                    density.sd = 0.6042731,
                    cvisit.sep = 0.25,
                    cvisit.sd = 0.036
-                 ), Pcomp = 1, Bcomp = 1, Bsen = 1) {
+                 ), Pcomp = 1, Bcomp = 1) {
 
   # Preparation
   f.org <- JMbayes:::gaussKronrod()$sk
@@ -137,13 +158,9 @@ asim <- function(n = 1000, seed = 100,
     rbinom(1, 1, h(trueTimes[i], 2, i)/(h(trueTimes[i], 2, i) + h(trueTimes[i], 1, i))) + 1  # in rbinom 1 = event 2
   })
 
-  # simulate censoring times from an exponential distribution,
+  # simulate censoring times from an uniform distribution,
   # and calculate the observed event times, i.e., min(true event times, censoring times)
   Ctimes <- runif(n, 0, 2 * mean.Cens)
-  Time <- pmin(Ctimes, trueTimes)
-
-  event <- sapply(1:n, function(i) {ifelse(trueTimes[i] < Ctimes[i], event.2[i], 0)}) # event indicator
-
   fixed_visits <- matrix(times, ncol = n)
   fv_idx <- c(1,3,5, seq(9,K, 8))
   if (K %in% fv_idx) {
@@ -151,6 +168,37 @@ asim <- function(n = 1000, seed = 100,
   } else {
     fixed_visits <- fixed_visits[c(fv_idx, K),]
   }
+  fixed_visits_cmpl <- matrix(rbinom(length(fixed_visits), 1, Bcomp),
+                         nrow(fixed_visits), ncol(fixed_visits)) # the compliance rate of biopsies
+  fixed_visits <- fixed_visits * fixed_visits_cmpl # updated biopsies with compliance rate
+
+  Time.prg <- sapply(1:n, function(i) {ifelse(event.2[i] == 1, trueTimes[i], NA)})
+  Time.prg_obs <- sapply(1:n, function(i) {
+    if(event.2[i] == 1) {
+      if(sum(fixed_visits[,i] >= trueTimes[i]) == 0) {
+        max(fixed_visits[,i])
+      } else {
+        min(fixed_visits[fixed_visits[,i] >= trueTimes[i],i])
+      }
+    } else {
+      NA
+    }
+  })
+  Time.trt <- sapply(1:n, function(i) {ifelse(event.2[i] == 2, trueTimes[i], NA)})
+  Time.cen <- Ctimes
+  Time <- pmin(Ctimes, Time.prg_obs, Time.trt, na.rm = T)
+
+  event <- sapply(1:n, function(i) {ifelse(max(Time.prg_obs[i], Time.trt[i], na.rm = T) < Ctimes[i], event.2[i], 0)}) # event indicator
+
+  Time[event == 1] <- Time.prg[event == 1]
+
+  # fixed_visits <- matrix(times, ncol = n)
+  # fv_idx <- c(1,3,5, seq(9,K, 8))
+  # if (K %in% fv_idx) {
+  #   fixed_visits <- fixed_visits[fv_idx,]
+  # } else {
+  #   fixed_visits <- fixed_visits[c(fv_idx, K),]
+  # }
 
   Time1 <- sapply(1:n, function(i) {
     max(fixed_visits[fixed_visits[,i] <= Time[i],i])
@@ -172,6 +220,10 @@ asim <- function(n = 1000, seed = 100,
     }
   })
 
+  # add compliance rate for PSA
+  ind.cmpl <- rbinom(length(ind), 1, Pcomp)
+  ind <- matrix(as.logical(ind * ind.cmpl), nrow(ind), ncol(ind))
+
   y.obs <- y[ind]
 
   id.obs <- id[ind]
@@ -189,10 +241,13 @@ asim <- function(n = 1000, seed = 100,
   dat$b2 <- b[id.obs,2]
   dat$b3 <- b[id.obs,3]
   dat$b4 <- b[id.obs,4]
+  dat$time.prg <- Time.prg[id.obs]
+  dat$time.trt <- Time.trt[id.obs]
+  dat$time.cen <- Time.cen[id.obs]
 
   dat <- dat[c("CISNET_ID", "TimeSince_Dx", "PSAValue", "time", "time.cmp1",
                "time.cmp2","status.cmp", "density","DxAge",
-               "b1", "b2", "b3", "b4")]
+               "b1", "b2", "b3", "b4", "time.prg", "time.trt", "time.cen")]
 
   dat.id <- dat[!duplicated(dat$CISNET_ID), ]
 
